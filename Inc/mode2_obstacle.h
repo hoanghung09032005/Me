a703494ecc31a76f6e42de93f96db081d53ee2fe
hoanghung09032005 <23020820@vnu.edu.vn>
2026-08-12 20:53:54 +0700

    Delete Inc directory

--- Inc/mode2_obstacle.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODE2_OBSTACLE_H
-#define MODE2_OBSTACLE_H
-
-#include <stdint.h>
-
-void Mode2_Obstacle_Init(void);
-void Mode2_Obstacle_Update(uint8_t raw_state, uint8_t side_left, uint8_t side_right);
-
-#endif // MODE2_OBSTACLE_H
